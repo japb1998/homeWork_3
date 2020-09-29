87219eb56340b1121f678784082387d2bc5bba94
Javier <javierperez@javiers-mbp.home>
2020-09-29 15:23:32 -0400

    it works

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var criteria_5= prompt('how long do you want it to be ?  from 8 to 128');
 var criteria_1= confirm('UpperCase ?');
 var criteria_2= confirm('LowerCase ?');
 var criteria_3= confirm('Numbers ?');
 var criteria_4= confirm('Symbols ?');
-var criteria_5= prompt('how long do you want it to be ?  from 8- to 128');
 
+function test(){
+if( parseInt(criteria_5) < 8 || parseInt(criteria_5) > 128){ 
 
+  criteria_5= prompt('it need to be 8-128');
+  test();
+}
+};
+test();
 
 // Write password to the #password input
 function writePassword() {
@@ -20,7 +27,7 @@ for(var i=0 ; i < parseInt(criteria_5) ; i++){
   var wantUpper = String.fromCharCode(Math.floor(Math.random()*26)+65);
   var wantLower = String.fromCharCode(Math.floor(Math.random()*26)+97) ;
   var wantNumber = String.fromCharCode(Math.floor(Math.random()*10)+48) ;
-  var symbols=' "!#$%&()*+,-./:;<=>?@[\]^_`{|}~ ' ;
+  var symbols='"!#$%&()*+,-./:;<=>?@[\]^_`{|}~' ;
   var wantSymbols= symbols[Math.floor(Math.random() * symbols.length)];
   
 if(criteria_1){ passwordArray.push(wantUpper); }
