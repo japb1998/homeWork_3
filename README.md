cbeafa410b0e93074a1435fdf2b4479420898776
Javier <javierperez@javiers-mbp.home>
2020-10-03 13:23:09 -0400

    it finally works

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # homeWork_3
+
+<hr>
+
+## My Respository
+this is the repo where all the magic is happening for the password generator.
+### created by Javier Perez
+On this homeWork we are generating a random password, asking our user what to take in consideration: 
+* UpperCase 
+* LowerCase
+* Numbers
+* symbols
+## Files
+contains an index.html file with the structure of my app, a style.css giving the styling to my app and we have a script.js generating a random 
+password.
+
+
+
+
+## APP
+![screenshot of the app](./demo/img1.png)
+## options: it would ask you what you want to include and how many characters you want through prompts
+![scrennshot of how the prompts are display after you click generate](./demo/img2.png)
+ ## Result
+ ![generate Password](./demo/img3.png)
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,45 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var criteria_5= prompt('how long do you want it to be ?  from 8 to 128');
-var criteria_1= confirm('UpperCase ?');
-var criteria_2= confirm('LowerCase ?');
-var criteria_3= confirm('Numbers ?');
-var criteria_4= confirm('Symbols ?');
-
-function test(){
-if( parseInt(criteria_5) < 8 || parseInt(criteria_5) > 128){ 
-
-  criteria_5= prompt('it need to be 8-128');
-  test();
-}
-};
-test();
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-passwordText.value = password;
+  passwordText.value = password;
 
- function generatePassword (){ 
-  var passwordArray=[];
-for(var i=0 ; i < parseInt(criteria_5) ; i++){
-  var wantUpper = String.fromCharCode(Math.floor(Math.random()*26)+65);
-  var wantLower = String.fromCharCode(Math.floor(Math.random()*26)+97) ;
-  var wantNumber = String.fromCharCode(Math.floor(Math.random()*10)+48) ;
-  var symbols='"!#$%&()*+,-./:;<=>?@[\]^_`{|}~' ;
-  var wantSymbols= symbols[Math.floor(Math.random() * symbols.length)];
-  
-if(criteria_1){ passwordArray.push(wantUpper); }
-if(criteria_2){ passwordArray.push(wantLower); }
-if(criteria_3){ passwordArray.push(wantNumber); }
-if(criteria_4){ passwordArray.push(wantSymbols); }
- passwordArray=  passwordArray.slice(0,parseInt(criteria_5));
-}
 
-return passwordArray.join('');
- }
- 
+
+  function generatePassword() {
+    var criteria_5 = prompt('how long do you want it to be ?  from 8 to 128');
+    var criteria_1 = confirm('UpperCase ?');
+    var criteria_2 = confirm('LowerCase ?');
+    var criteria_3 = confirm('Numbers ?');
+    var criteria_4 = confirm('Symbols ?');
+
+    function test() {
+      if (parseInt(criteria_5) < 8 || parseInt(criteria_5) > 128) {
+
+        criteria_5 = prompt('it need to be 8-128');
+        test();
+      }
+    };
+    test();
+    var passwordArray = [];
+    // loop as many time as the user says with the criteria 5 value
+    for (var i = 0; i < parseInt(criteria_5); i++) {
+      var wantUpper = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+      var wantLower = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+      var wantNumber = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+      var symbols = '"!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+      var wantSymbols = symbols[Math.floor(Math.random() * symbols.length)];
+
+      if (criteria_1) {
+        passwordArray.push(wantUpper);
+      }
+      if (criteria_2) {
+        passwordArray.push(wantLower);
+      }
+      if (criteria_3) {
+        passwordArray.push(wantNumber);
+      }
+      if (criteria_4) {
+        passwordArray.push(wantSymbols);
+      }
+//slice my password in the number of values i need
+      passwordArray = passwordArray.slice(0, parseInt(criteria_5));
+    }
+//make my array a string
+    return passwordArray.join('');
+  }
+
 
 }
 
